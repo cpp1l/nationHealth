243ce492f71e93659ae8e8428227897421e75103
Vitaliy-1 <vitaliybezsh@gmail.com>
2026-05-24 13:22:54 +0300

    Pagination styling

--- resources/views/livewire/declaration/declaration-index.blade.php
+++ resources/views/livewire/declaration/declaration-index.blade.php
@@ -239,7 +239,7 @@
                         </table>
                 </div>
 
-                <div class="mt-8 pl-3.5 pb-8 lg:pl-8 2xl:pl-5">
+                <div class="pagination">
                     {{ $declarations->links() }}
                 </div>
             @else
